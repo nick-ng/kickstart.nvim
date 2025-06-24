1a6e873158e938383eb063ba01c7e879ec69d688
nick-ng <nick.ng@fergus.com>
2025-06-24 17:10:45 +1200

    feat: tried psalm. went back to intelephense

--- init.lua
+++ init.lua
@@ -688,6 +688,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
         intelephense = {},
+        -- psalm = {},
 
         lua_ls = {
           -- cmd = { ... },
