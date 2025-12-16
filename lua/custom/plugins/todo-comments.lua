ea148c2b35623ee28c2d0cae085046031fb2d5b3
pux <nick-ng@users.noreply.github.com>
2025-12-16 21:07:33 +1300

    feat: highlight my style of comments

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -1,2 +1,35 @@
 -- Highlight todo, notes, etc in comments
-return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+
+-- @todo(nick-ng): test todo
+
+-- @TODO(nick-ng): test todo
+
+-- TODO: Test
+
+-- todo: test
+return {
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    signs = false,
+    keywords = {
+      todo = { icon = ' ', color = 'info' },
+    },
+    highlight = {
+      multiline = true, -- enable multine todo comments
+      multiline_pattern = '^.', -- lua pattern to match the next multiline from the start of the matched keyword
+      multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+      before = 'fg', -- "fg" or "bg" or empty
+      keyword = 'bg', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      after = 'fg', -- "fg" or "bg" or empty
+      pattern = {
+        [[\@(KEYWORDS)\(.*\):]],
+        [[.*<(KEYWORDS)\s*:]],
+      }, -- pattern or table of patterns, used for highlighting (vim regex)
+      comments_only = true, -- uses treesitter to match keywords in comments only
+      max_line_len = 400, -- ignore lines longer than this
+      exclude = {}, -- list of file types to exclude highlighting
+    },
+  },
+}
