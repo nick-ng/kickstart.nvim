9017d86c7f2914c2603e39fc6d2faf6b32b42825
pux <nick-ng@users.noreply.github.com>
2025-12-24 11:17:16 +1300

    feat: highlight entire todo comment with background style

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -16,13 +16,14 @@ return {
     keywords = {
       todo = { icon = ' ', color = 'info' },
     },
+    merge_keywords = true,
     highlight = {
       multiline = true, -- enable multine todo comments
       multiline_pattern = '^.', -- lua pattern to match the next multiline from the start of the matched keyword
       multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
       before = 'fg', -- "fg" or "bg" or empty
-      keyword = 'bg', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-      after = 'fg', -- "fg" or "bg" or empty
+      keyword = 'wide_bg', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      after = 'bg', -- "fg" or "bg" or empty
       pattern = {
         [[\@(KEYWORDS)\(.*\):]],
         [[.*<(KEYWORDS)\s*:]],
